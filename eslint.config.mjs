240a3a183bb85145c178acb40218c0d9cdf501fa
Sam Davis <sam@sjd.co>
2024-09-03 18:11:53 +1000

    Add lint step

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ export default [
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     {
-        ignores: ["dist/"],
+        files: ["src/**/*"],
         rules: {
             "@typescript-eslint/ban-ts-comment": "off",
             "@typescript-eslint/no-explicit-any": "off",
